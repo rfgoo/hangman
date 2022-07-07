db42ff153c2b851ba1f78589e62583df42201f77
rfgoo <rfgoo@iscte-iul.pt>
2022-07-07 14:16:20 +0100

    added images to make the body in ui

--- ui.py
+++ ui.py
@@ -12,12 +12,18 @@ clock = pg.time.Clock()
 
 screen = pg.display.set_mode((600, 600))
 pg.display.set_caption("Hangman")
-forca = pg.transform.scale(pg.image.load("hangman.png"),(200,200))
+forca = pg.transform.scale(pg.image.load("hangman.png"),(300, 300))
+head = pg.transform.scale(pg.image.load("head.png"), (64, 64))
+torso = pg.transform.scale(pg.image.load("torso.png"), (64, 64))
+l_arm = pg.transform.scale(pg.image.load("l_arm.png"), (40, 40))
+r_arm = pg.transform.flip(l_arm, True, False)
+l_leg = pg.transform.scale(pg.image.load("leg.png"), (60, 60))
+r_leg = pg.transform.flip(l_leg, True, False)
 
 label_lives = main_font.render(f"Lives: {game.lives}/6",True,(255,255,255))
 label_word = main_font.render(' '.join(game.letters), True, (255,255,255))
 label_failed = main_font.render(f"Failled attempts: {', '.join(game.failed)}", True, (255, 255, 255))
-label2= main_font.render(f"The secret word is {game.word}", True, (255,255,255))
+label2 = main_font.render(f"The secret word is {game.word}", True, (255, 255, 255))
 
 run = True
 status = None
@@ -26,8 +32,21 @@ while run:
     if status is None:
         screen.fill(BG_COLOR)
         input.update(events)
-        screen.blit(forca, (200, 200))
+        screen.blit(forca, (150, 100))
         screen.blit(input.surface, (10, 10))
+
+    if game.lives >= 1:
+        screen.blit(head, (355,185))
+    if game.lives >= 2:
+        screen.blit(torso,(355,185+63))
+    if game.lives >= 3:
+        screen.blit(l_arm, (323, 185 + 50))
+    if game.lives >= 4:
+        screen.blit(r_arm, (326+85, 185 + 50))
+    if game.lives >= 5:
+        screen.blit(l_leg, (323+50, 185 + 50+50+25))
+    if game.lives >= 6:
+        screen.blit(r_leg, (323+20, 185 +50+50+25))
 
     for event in events:
         if event.type == pg.QUIT:
@@ -46,7 +65,6 @@ while run:
     screen.blit(label_lives,(0,550))
     screen.blit(label_word, (200, 400))
     screen.blit(label_failed, (165, 550))
-            # TODO desenhar o boneco no ecra com base no numero de tentativas falhadas
 
     if status == True or status == False:
         if status == True:
@@ -54,9 +72,7 @@ while run:
         else:
             label = main_font.render(f"LOOOOOOSER!!!", True, (255,255,255))
 
-        screen.blit(label, (150, 50))
-        screen.blit(label2, (150, 100))
+        screen.blit(label, (150, 10))
+        screen.blit(label2, (150, 60))
     pg.display.update()
     clock.tick(60)
-
-
